653495867b2d5a7301181175982fbeb31576e849
Miguel Martin <miguelmartin@Mac-mini-de-Miguel.local>
2024-11-15 15:59:57 +0100

    addTest

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,11 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("The function should return undefined if any of the two arguments is not a number.", () => {
+        expect(add("a", 2)).toEqual(undefined);
+        expect(add(2, "b")).toEqual(undefined);
+        expect(add("a", "b")).toEqual(undefined);
+      });
     });
   });
   
